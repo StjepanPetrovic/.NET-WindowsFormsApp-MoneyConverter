44dcddbe9ff6d1809d1e05a8e09ad9b897bd31cb
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2022-10-10 19:23:09 +0200

    Validacija - upozoravanje korisnika na neispravan unos

--- MoneyConverter/FormMoneyConv.cs
+++ MoneyConverter/FormMoneyConv.cs
@@ -22,13 +22,32 @@ namespace MoneyConverter
             Converter converter = new Converter
             {
                 FirstCurrency = Converter.getCurrency(comboBox1.Text),
-                SecondCurrency = Converter.getCurrency(comboBox2.Text),
-                Amount = Convert.ToDouble(textBox1.Text)
+                SecondCurrency = Converter.getCurrency(comboBox2.Text)
             };
 
-            converter.ConvertedAmount = (converter.Amount * converter.FirstCurrency.Tecaj) / converter.SecondCurrency.Tecaj;
+            double input = converter.Amount;
 
-            textBox2.Text = converter.ConvertedAmount.ToString();
+            if (Double.TryParse(textBox1.Text, out input))
+            {
+                if (Double.Parse(textBox1.Text) < 0)
+                {
+                    MessageBox.Show("Please, input positive value.");
+                    textBox2.Text = "";
+                }
+                else
+                {
+                    converter.Amount = Convert.ToDouble(textBox1.Text);
+
+                    converter.ConvertedAmount = (converter.Amount * converter.FirstCurrency.Tecaj) / converter.SecondCurrency.Tecaj;
+                    textBox2.Text = converter.ConvertedAmount.ToString();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please, input positive value without letters.");
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
         }
     }
 }
