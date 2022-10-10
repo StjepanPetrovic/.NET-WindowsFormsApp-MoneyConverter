0ee310a8565d0861a3a4b8aa8de82fe037dc231b
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2022-10-10 15:33:38 +0200

    Implementirane klase poslovne logike

---
+++ MoneyConverter/Converter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyConverter
+{
+    public class Converter
+    {
+        public double FirstCurrency { get; set; }
+        public double SecondCurrency { get; set; }
+        public double Amount { get; set; }
+        public double ConvertedAmount { get; set; }
+
+        public static List<Currency> currencies = new List<Currency>
+        {
+            new Currency
+            {
+                Valuta = "HRK",
+                Tecaj = 1
+            },
+            new Currency
+            {
+                Valuta = "EUR",
+                Tecaj = 7.4
+            },
+            new Currency
+            {
+                Valuta = "USD",
+                Tecaj = 6.5
+            },
+            new Currency
+            {
+                Valuta = "GBP",
+                Tecaj = 8.5
+            },
+            new Currency
+            {
+                Valuta = "CAD",
+                Tecaj = 4.9
+            }
+        };
+    }
+}
---
+++ MoneyConverter/Currency.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyConverter
+{
+    public class Currency
+    {
+        public string Valuta { get; set; }
+        public double Tecaj { get; set; }
+    }
+}
